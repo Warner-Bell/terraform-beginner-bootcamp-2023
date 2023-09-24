674ff178a197e39d6f864472dd2a84abc3d0069a
Warner Bell <warner.bell21@gmail.com>
2023-09-24 17:14:46 +0000

    #28 refactor s3 to new terrahoouse mod

---
+++ modules/terrahouse_aws/main.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+
+
+                            # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+                            ## Bucket Naming Rules
+                            #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+  #region = "us-east-1"  # Update the region here
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "bucket_name" {
-  value = aws_s3_bucket.website_bucket
+  description = "Bucket name for the static website"
+  value = module.terrahouse_aws.bucket_name
 }
