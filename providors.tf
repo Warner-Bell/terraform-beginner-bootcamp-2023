9a0c727296268dcfa33c52ea0883f86b97a4738b
Warner Bell <100949697+Warner-Bell@users.noreply.github.com>
2023-09-24 11:24:48 -0500

    #28 bucket nolonger used rado providor (#29)

--- providors.tf
+++ providors.tf
@@ -9,13 +9,9 @@ terraform {
 # }
 
 required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source = "hashicorp/aws"
-      version = "5.16.2"
+      version = "5.17.0"
     }
   }
 }
