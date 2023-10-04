f213e6408c08eb32fe235737cc2885082361f998
Warner Bell <warner.bell21@gmail.com>
2023-10-04 11:12:58 +0000

    #44  implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,28 +1,36 @@
 terraform {
 #   cloud {
 #     organization = "Gadgetools_Unlimited"
-
 #     workspaces {
 #       name = "Terra-House"
 #     }
 #   }
-# }
 
 required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
     }
   }
-}
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "Gadgetools_Unlimited"
+
+ }
 
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  assets_path = var.assets_path
-  content_version = var.content_version
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
+
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   assets_path = var.assets_path
+#   content_version = var.content_version
+# }
