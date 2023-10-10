a24c12c707cf315ed86e6d2aafc1a8466f702cbe
Warner Bell <warner.bell21@gmail.com>
2023-10-10 02:45:58 +0000

    #52 fixxed provoder issues ect.

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 terraform {
-#   cloud {
-#     organization = "Gadgetools_Unlimited"
-#     workspaces {
-#       name = "Terra-House"
-#     }
-#   }
+  cloud {
+    organization = "Gadgetools_Unlimited"
+    workspaces {
+      name = "Terra-House-2"
+    }
+  }
+  #backend "remote" {
+    #  hostname = "app.terraform.io"
+    #  organization = "Gadgetools_Unlimited"
 
 required_providers {
     terratowns = {
@@ -12,10 +15,6 @@ required_providers {
       version = "1.0.0"
     }
   }
-  #backend "remote" {
-  #  hostname = "app.terraform.io"
-  #  organization = "Gadgetools_Unlimited"
-
  }
 
 
@@ -25,26 +24,44 @@ provider "terratowns" {
   token = var.terratowns_access_token
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
+module "home_soultrek_hosting" {
+  source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
+  public_path = var.soultrek.public_path
   bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  assets_path = var.assets_path
-  content_version = var.content_version
+  content_version = var.soultrek.content_version
 }
 
 resource "terratowns_home" "home" {
-  name = "SoulTrek 2023!"
+  name = "SoulTrek 2023"
   description = <<DESCRIPTION
 Soul Trek is a meme style pop culture parody adaptation of the science fiction tv series Star Trek TNG,
 that followed the crew of a new starship Enterprise a century after the original series.
 Soul Trek a series of images spoofing the characters and themes of the TNG tv series, explores the world
 of intergalactic space travel with an element of funk. Images Created by Karter AI Artz.
 DESCRIPTION
-  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = module.home_soultrek_hosting.domain_name
   #domain_name = "4ytq3gz.cloudfront.net"
   town = "missingo"
-  content_version = 1
+  content_version = var.soultrek.content_version
+}
+
+
+module "home_rccars_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.rccars.public_path
+  bucket_name = var.bucket_name
+  content_version = var.rccars.content_version
+}
+
+resource "terratowns_home" "home_rccars" {
+  name = "RC Cars & Trucks"
+  description = <<DESCRIPTION
+There are some really cool and fun rc vehicles out there these days.
+DESCRIPTION
+  domain_name = module.home_rccars_hosting.domain_name
+  #domain_name = "4ytq3gz.cloudfront.net"
+  town = "missingo"
+  content_version = var.rccars.content_version
 }
